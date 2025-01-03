fc5ede6fd409b2e8250f5fe7e2b1d676eca8cad2
Miguel Ramos <return.infox@gmail.com>
2025-01-03 09:42:33 -0300

    circuit break fallback

---
+++ src/main/java/com/kibernumacademy/companyfallback/CompanyfallbackApplication.java
@@ -0,0 +1,13 @@
+package com.kibernumacademy.companyfallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyfallbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyfallbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kibernumacademy/companyfallback/CompanyfallbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kibernumacademy.companyfallback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyfallbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
